f186a10786e31d0d87a155e504bf3a8226ace281
Daniyal Ahmed <da9033431@gmail.com>
2026-01-21 15:42:57 +0000

    feat: tools setup

--- index.ts
+++ index.ts
@@ -1,19 +1,23 @@
 import { ChatGroq } from "@langchain/groq";
+import { createEventTool, getEventsTool } from "./tools";
 
-const tools: any = [];
+const tools = [createEventTool, getEventsTool];
 
 const llm = new ChatGroq({
   model: "llama-3.3-70b-versatile",
   temperature: 0,
 }).bindTools(tools);
 
-llm.invoke([
-  {
-    role: "user",
-    content: "What is the capital of France?"
-  }
-]).then((response) => {
-  console.log("Response:", response);
-}).catch((error) => {
-  console.error("Error:", error);
-});
+llm
+  .invoke([
+    {
+      role: "user",
+      content: "What is the capital of France?",
+    },
+  ])
+  .then((response) => {
+    console.log("Response:", response);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
---
+++ tools.ts
@@ -0,0 +1,28 @@
+import * as z from "zod";
+import { tool } from "langchain";
+
+export const createEventTool = tool(
+  async ({}) => `Created event in the calendar.`,
+  {
+    name: "create_event",
+    description: "Create a new event in the calendar.",
+    schema: z.object({
+      //   title: z.string().describe("Title of the event"),
+      //   date: z.string().describe("Date of the event"),
+      //   location: z.string().describe("Location of the event"),
+    }),
+  },
+);
+
+export const getEventsTool = tool(
+  async ({}) => `Retrieved events from the calendar.`,
+  {
+    name: "get_events",
+    description: "Retrieve events from the calendar.",
+    schema: z.object({
+      //   title: z.string().describe("Title of the event"),
+      //   date: z.string().describe("Date of the event"),
+      //   location: z.string().describe("Location of the event"),
+    }),
+  },
+);
